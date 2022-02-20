67a6ddec1960d7a9db572228a53b557d7650ab04
sairuk <sairuk@gmail.com>
2022-02-20 14:02:35 +1100

    theorised docker implementation

--- dialog/experimental.sh
+++ dialog/experimental.sh
@@ -11,6 +11,7 @@ rn_advanced() {
   local MENU_ARRAY=(
     01 "Back"
     02 "webui - experimental retronas webui (cockpit)"
+    03 "piforce (docker) - (rpi4) >15m install, >2GB space req."
   )
 
   local MENU_BLURB="\nWARNING these tools are very EXPERIMENTAL and could break your system or destroy your data! Do not use these unless you are comfortable with losing everthing"
@@ -33,6 +34,14 @@ do
     CLEAR
     RN_INSTALL_DEPS
     RN_INSTALL_EXECUTE install_cockpit-retronas.yml
+    PAUSE
+    ;;
+  03)
+    # experimental webui
+    CLEAR
+    RN_INSTALL_DEPS
+    RN_INSTALL_EXECUTE install_docker.yml
+    RN_INSTALL_EXECUTE install_piforce.yml
     PAUSE
     ;;
   *)
